2e349979406d9c0047cba0af79ba6b78c8be145f
Heri Hermawan <herhermawan007@gmail.com>
2019-04-11 11:55:34 +0700

    udpate https try

--- server.js
+++ server.js
@@ -6,6 +6,16 @@ const port = process.env.PORT || 5000;
 //force https
 app.set('trust proxy', true);
 
+function requireHTTPS(req, res, next) {
+  // The 'x-forwarded-proto' check is for Heroku
+  if (!req.secure && req.get('x-forwarded-proto') !== 'https' && process.env.NODE_ENV !== "development") {
+    return res.redirect('https://' + req.get('host') + req.url);
+  }
+  next();
+}
+
+app.use(requireHTTPS);
+
 //Static file declaration
 app.use(express.static(path.join(__dirname, 'build')));
 
